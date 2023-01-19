8620872ef82ec6e8bab1fe457c58d4aec6fb4e0b
Sandesh Jain <sandeshjain2507@gmail.com>
2023-01-19 22:27:33 +0530

    Router Updates

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,10 @@
-import express from "express";
+import express, { application } from "express";
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    console.log("Ga");
+    return  res.send("<h1>Hello World</h1>");
+})
 
 export default router;
