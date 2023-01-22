f5635efe958c00e5391da20f7acfb79f9176e317
Sandesh Jain <81623367+SandeshJ-07@users.noreply.github.com>
2023-01-22 13:52:28 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -3,8 +3,7 @@ import express, { application } from "express";
 const router = express.Router();
 
 router.get("/try", (req, res) => {
-    console.log("Ga");
-    return  res.json("Hello World");
+    res.json("Hello World");
 })
 
 export default router;
